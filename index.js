7b334e69927b0d33e5902290ae9d754f60a6438a
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-05 07:26:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getRepositories() {
 function displayRepositories() {
   const repos = JSON.parse(this.responseText)
   const repoList = "<ul>" + repos.map(repo => {
-    const dataUsername = 'data-username="' + repo.owner + '"'
+    const dataUsername = 'data-username="' + repo.owner.login + '"'
     const dataRepoName = 'data-repository="' + repo.name + '"'
     return(`
           <li>
